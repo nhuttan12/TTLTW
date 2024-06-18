a9bdd1e17d849b031bf414d41087dbfd324d6244
Hoài Thu <21130553@st.hcmuaf.edu.vn>
2024-06-19 06:46:33 +0700

    thêm chức năng đăng nhập bằng google

---
+++ test10/src/main/java/login/Google.java
@@ -0,0 +1,11 @@
+package login;
+
+public class Google {
+	public static String GOOGLE_CLIENT_ID = "1089316008342-fllevmop2b90sd7mmsqotdo1neb934sg.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/test10/loginGoogle";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v3/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+
+}
