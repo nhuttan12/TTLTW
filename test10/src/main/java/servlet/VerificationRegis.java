cd8ee5d3320512235f8ea497c27b8cb4b5eb89a2
Hoài Thu <21130553@st.hcmuaf.edu.vn>
2024-06-19 06:36:29 +0700

    sửa lại chức năng quên mật khẩu + gửi mail ghi quên mật khẩu hoặc khi đăng ký tài khoản

--- test10/src/main/java/servlet/VerificationRegis.java
+++ test10/src/main/java/servlet/VerificationRegis.java
@@ -39,7 +39,7 @@ public class VerificationRegis extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		HttpSession httpSession =req.getSession();
+    	HttpSession httpSession =req.getSession();
 		String verification = null;
 
     	try {
@@ -53,43 +53,46 @@ public class VerificationRegis extends HttpServlet {
 		String PASSWORD = (String) httpSession.getAttribute("PASSWORD");
 		String NAME = (String) httpSession.getAttribute("NAME");
 		String PHONE = (String) httpSession.getAttribute("PHONE");
-		String GENDER = (String) httpSession.getAttribute("GENDER");
+		String GENDER1 = (String) httpSession.getAttribute("GENDER");
+		int GENDER = Integer.parseInt(GENDER1);
 		String EMAIL = (String) httpSession.getAttribute("EMAIL");
 		String ver = (String) httpSession.getAttribute("ver");
 //		System.out.println("pass moi " +req.getAttribute("newpas"));
 //		System.out.println("ver moi " +ver);
 
 		
-//		if(verification.equals(ver)) {
-//			DBUser l = new DBUser();
-//	   		DBShoppingCart cart = new DBShoppingCart();
-//			try {
-//   				User a = new User(USER_NAME, PASSWORD, NAME, PHONE, GENDER,EMAIL);
-//				l.addUSER(a);
-//				a.setId(l.getUserId(a));
-//				a.setRole(1);
-//				a.setStatus(1);
-//				System.out.println(l.getUserId(a));
-//				ShoppingCart s = new ShoppingCart(a);
+		if(verification.equals(ver)) {
+			DBUser l = new DBUser();
+	   		DBCart cart = new DBCart();
+			try {
+   				User a = new User(USER_NAME, PASSWORD, NAME, PHONE, GENDER,EMAIL);
+   				System.out.println("aaaaaaaaaaaaaaaaaaaaaa" + a.getName());
+				l.addUSER(a);
+				a.setId(l.getUserId(a));
+				a.setRole(1);
+				a.setStatus(1);
+				System.out.println(l.getUserId(a));
+//				Cart s = new Cart(a);
 //				cart.addShoppingCart(s);
 //				s.setId(cart.getCartId(a.getId()));
 //				l.updateCart(a, s.getId());
-//	   			httpSession.setAttribute("user", a);
+	   			httpSession.setAttribute("user", a);
 //	   			httpSession.setAttribute("cart", s);
-//	   			RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
-//	   			dispatcher.forward(req, resp);
-//	   			//req.getRequestDispatcher("login.jsp").forward(req, resp);
-//			} catch (SQLException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-//
-//		}else {
-//			 System.out.println("ma khong dung");
-//			 RequestDispatcher dispatcher = req.getRequestDispatcher("verification.jsp");
-//				dispatcher.forward(req, resp);
-//		}
+//	   			System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+	   			RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+	   			dispatcher.forward(req, resp);
+	   			//req.getRequestDispatcher("login.jsp").forward(req, resp);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
+		}else {
+			 System.out.println("ma khong dung");
+			 RequestDispatcher dispatcher = req.getRequestDispatcher("verificationRegis.jsp");
+				dispatcher.forward(req, resp);
+		}
     }
 
 }
