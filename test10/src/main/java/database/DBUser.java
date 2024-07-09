45530565cbec3c3df93844f1655a079a177a3bf2
Ngọc Thảo <21130542@st.hcmuaf.edu.vn>
2024-07-09 13:15:06 +0700

    fix thay doi thong tin user

--- test10/src/main/java/database/DBUser.java
+++ test10/src/main/java/database/DBUser.java
@@ -327,6 +327,29 @@ public class DBUser {
 
 		return status;
 	}
+	public int updateInfor(int id, String fullname,int phone, String gender) throws SQLException {
+		int status = 0;
+
+		try (Connection c = connectionDB.connect()) {
+			User a = getUserByID(id);
+			if (a != null) {
+				String sql = "UPDATE USERS SET FULL_NAME = ?, PHONE = ?, GENDER = ? WHERE USER_ID = ?;";
+				PreparedStatement ps = c.prepareStatement(sql);
+				ps.setString(1, fullname);
+				ps.setInt(2, phone);
+				ps.setString(3, gender);
+				ps.setInt(4, id);
+				status = ps.executeUpdate();
+			} else {
+				System.out.println("khong tim thay user");
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return status;
+	}
 
 	public User getUserByUserName(String usn) throws SQLException {
 		User b = null;
--- test10/src/main/java/servlet/VerificationPass.java
+++ test10/src/main/java/servlet/VerificationPass.java
@@ -7,10 +7,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import log.ErrorMessage;
+import log.InforMessage;
+import log.LevelLog;
+import log.MyLog;
+import model.Logging;
 import model.User;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 import database.DBUser;
 import email.ServerSendMail;
@@ -21,52 +29,55 @@ import email.ServerSendMail;
 @WebServlet("/verificationPass")
 public class VerificationPass extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public VerificationPass() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-HttpSession httpSession=req.getSession();
-		
-		User user=(User) httpSession.getAttribute("user");
-		if(user==null) {
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public VerificationPass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession httpSession = req.getSession();
+		Logging logging =null;
+		User user = (User) httpSession.getAttribute("user");
+		if (user == null) {
 			req.setAttribute("erro", "bạn phải đăng nhập !");
-			req.getRequestDispatcher("login.jsp").forward(req, resp);		}else {     	
-		String verification = req.getParameter("verification");
+			req.getRequestDispatcher("login.jsp").forward(req, resp);
+		} else {
+			String verification = req.getParameter("verification");
 
-		int uid = user.getId();
-		System.out.println("id user " +uid);
-		
-		
-		String newpas = (String) httpSession.getAttribute("newpas");
-		String ver = (String) httpSession.getAttribute("ver");
+			int uid = user.getId();
+			System.out.println("id user " + uid);
 
-		System.out.println("pass moi " +req.getAttribute("newpas"));
-		System.out.println("ver moi " +ver);
+			String newpas = (String) httpSession.getAttribute("newpas");
+			String ver = (String) httpSession.getAttribute("ver");
+			String oldpass=(String)httpSession.getAttribute("oldpass");
+			System.out.println("pass moi " + req.getAttribute("newpas"));
+			System.out.println("ver moi " + ver);
 
-		if(verification.equals(ver)) {
-			DBUser db = new DBUser();
-			try {
-				db.updatePas(uid, newpas);
-				System.out.println("da update pas");
-				RequestDispatcher dispatcher = req.getRequestDispatcher("index");
+			if (verification.equals(ver)) {
+				DBUser db = new DBUser();
+				try{
+					db.updatePas(uid, newpas);
+					System.out.println("da update pas");
+					logging=new Logging(LevelLog.INFOR.name(),InforMessage.THAY_DOI_MAT_KHAU_THANH_CONG.name(),oldpass,newpas);
+					RequestDispatcher dispatcher = req.getRequestDispatcher("user");
+					dispatcher.forward(req, resp);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else {
+				System.out.println("ma khong dung");
+				logging=new Logging(LevelLog.ERROR.name(),ErrorMessage.THAY_DOI_MAT_KHAU_THAT_BAI_MA_XAC_THUC_KHONG_DUNG.name(),oldpass,newpas);
+				RequestDispatcher dispatcher = req.getRequestDispatcher("verificationPass.jsp");
 				dispatcher.forward(req, resp);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-			
-
-		}else {
-			 System.out.println("ma khong dung");
-			 RequestDispatcher dispatcher = req.getRequestDispatcher("verificationPass.jsp");
-				dispatcher.forward(req, resp);
 		}
-    }
-    }
+		MyLog.insertLog(logging, req);
+	}
 }
